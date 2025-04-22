165a428ba91649dfa2cf424e7fe6732a0bd45e4e
DESKTOP-FM3S11H\Vdoxx <rupesh@virtualdoxx.com>
2025-04-22 17:19:45 +0530

    glassfish report

--- JNDI_SessionBean_Example/remote-ejb-call/src/main/java/RemoteEjbCall.java
+++ JNDI_SessionBean_Example/remote-ejb-call/src/main/java/RemoteEjbCall.java
@@ -1,8 +1,10 @@
 
 import demo.HelloSessionInterface;
 import java.util.Hashtable;
+import java.util.Properties;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -16,8 +18,11 @@ public class RemoteEjbCall {
     public static String LOOK_UP_STRING_1 = "ejb:demo/ejb_impl/HelloSessionBean!demo.HelloSessionInterface";
     public static HelloSessionInterface helloSessionInterface;
 
+    public static String LOOK_UP_ON_GLASSFISH = "java:global/demo/ejb_impl/HelloSessionBean!demo.HelloSessionInterface";
+
     public static void main(String[] args) throws Exception {
-        lookupWithEjbBindingHelloSessionInterface();
+        //lookupWithEjbBindingHelloSessionInterface();
+        lookupOnGlassfish();
     }
 
     public static void lookupWithEjbBindingHelloSessionInterface() throws Exception {
@@ -38,6 +43,29 @@ public class RemoteEjbCall {
         System.out.println("Response from server: " + response);
 
         context.close();
+    }
+
+    public static void lookupOnGlassfish() {
+        try {
+            // JNDI properties for GlassFish
+            Properties props = new Properties();
+            props.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
+            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost"); // GlassFish server IP
+            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");       // GlassFish default IIOP port
+
+            // Create the InitialContext
+            Context ctx = new InitialContext(props);
+
+            // Lookup EJB
+            HelloSessionInterface bean = (HelloSessionInterface) ctx.lookup(LOOK_UP_ON_GLASSFISH);
+
+            // Call EJB method
+            String response = bean.getHello("ANOTHER JVM ");
+            System.out.println("Response from server: " + response);
+
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
     }
 
 }
